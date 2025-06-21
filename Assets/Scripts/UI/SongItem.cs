0399180b0361c764c1f7d42c914fa2d6bc872340
DuganChandler <duganchandler10@gmail.com>
2025-06-21 18:15:17 -0400

    song selection and confrim owkrint

---
+++ Assets/Scripts/UI/SongItem.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+public class SongItem : MonoBehaviour {
+    [SerializeField] public TextMeshProUGUI songName;
+    [SerializeField] public TextMeshProUGUI bpm;
+    [SerializeField] public TextMeshProUGUI composer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
