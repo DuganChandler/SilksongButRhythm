c9da2d8a9b877e5655e79200cbaec2cda5e50acf
DuganChandler <duganchandler10@gmail.com>
2025-06-21 02:07:02 -0400

    Chart Parser hasa been made

---
+++ Assets/Scripts/Chart.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chart {
+    public List<NoteData> notes = new();
+    public int bpm;
+    public string name;
+    public string composer;
+    public string length;
+}
