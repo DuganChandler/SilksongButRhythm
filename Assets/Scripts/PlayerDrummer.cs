03d9a7e4c0bdd3a098413f682b75efe31c57f7fd
PoyoMain <CLC9512@GMAIL.COM>
2025-06-21 19:26:25 -0400

    Base game works again! YIPPEE!

--- Assets/Scripts/PlayerDrummer.cs
+++ Assets/Scripts/PlayerDrummer.cs
@@ -41,10 +41,18 @@ public class PlayerDrummer : MonoBehaviour
         controls.FaceDirection.performed += FaceDirection;
     }
 
+    private void Up_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        if (!obj.started) return;
+
+        print("Hit button");
+    }
+
     private void OnDisable()
     {
         controls.Disable();
         controls.ActionDirection.performed -= ActionDirection;
+        controls.FaceDirection.performed -= FaceDirection;
     }
 
     private void FaceDirection(UnityEngine.InputSystem.InputAction.CallbackContext obj)
@@ -77,14 +85,14 @@ public class PlayerDrummer : MonoBehaviour
         };
     }
 
-    private void ActionDirection(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    public void ActionDirection(UnityEngine.InputSystem.InputAction.CallbackContext obj)
     {
         Vector2 directionalInput = obj.ReadValue<Vector2>();
         if (Mathf.Abs(directionalInput.x) != 1 && Mathf.Abs(directionalInput.y) != 1) return;
 
-        Direction actionDirection = Vector2ToDirection(directionalInput);
+        NoteType actionType = Vector2ToAction(directionalInput);
 
-        SpriteRenderers[DirectionToInt(facingDirection)].color = Colors[DirectionToInt(actionDirection)];
+        LaneManager.Instance.CheckHit(facingDirection, actionType);
     }
 
     private int DirectionToInt(Direction dir) => dir switch
@@ -102,6 +110,15 @@ public class PlayerDrummer : MonoBehaviour
         else if (vector == Vector2.down) return Direction.South;
         else if (vector == Vector2.left) return Direction.West;
         else if (vector == Vector2.right) return Direction.East;
+        else throw new System.NotImplementedException();
+    }
+
+    private NoteType Vector2ToAction(Vector2 vector)
+    {
+        if (vector == Vector2.up) return NoteType.Spray;
+        else if (vector == Vector2.down) return NoteType.Stomp;
+        else if (vector == Vector2.left) return NoteType.Swat;
+        else if (vector == Vector2.right) return NoteType.Bat;
         else throw new System.NotImplementedException();
     }
 }
