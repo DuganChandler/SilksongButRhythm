fbb22ba3e8fac5843c62ce391ac68a5a3c238d94
DuganChandler <duganchandler10@gmail.com>
2025-06-22 18:47:07 -0400

    main menu music

--- Assets/Scripts/UI/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour {
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip; 
 
     private void Awake() {
         Screen.SetResolution(1920, 1080, true);
+        audioSource.clip = audioClip;
+        audioSource.Play();
     }
     public void OnStart() {
        SceneManager.LoadScene("SongSelect"); 
+       audioSource.Stop();
     }
 
     public void OnQuit() {
